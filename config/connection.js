47ffe2f5d2b4f73186248508493096c4263c8418
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-08-18 15:21:53 -0700

    updated heroku jawsdb connection info

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'burgers_db'
-});
+
  
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'burgers_db'
+  });
+}
+
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
 });
+
 
 
 //export values and functions to be used in other files
